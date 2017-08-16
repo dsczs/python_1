4f8d724c8025ca2b9bf0ba78dd8aba9aa3146af6
dsczs <370637594@qq.com>
2017-08-16 16:54:55 +0800

    1

---
+++ com/dsczs/test/dict1.py
@@ -0,0 +1,8 @@
+dict1 = {}
+dict1['name'] = 'ab'
+dict1['age'] = 2
+print(dict1)
+
+dict2 = {"name":"h","age":55}
+
+print(dict2)
--- com/dsczs/test/list1.py
+++ com/dsczs/test/list1.py
@@ -1,3 +1,6 @@
-list_string = [1,2,3,'ab',bool]
+list_string = [1,2,3,'ab']
 print(list_string)
 print(list_string[0])
+
+for a in list_string:
+    print(a)
